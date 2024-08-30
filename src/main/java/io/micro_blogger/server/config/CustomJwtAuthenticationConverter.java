3434ec2c0de07ed3d3c52a3a5f103f261e6e6557
KittyCody <monikalna@gmail.com>
2024-08-30 18:21:45 +0200

    Security Configuration

---
+++ src/main/java/io/micro_blogger/server/config/CustomJwtAuthenticationConverter.java
@@ -0,0 +1,6 @@
+package io.micro_blogger.server.config;
+
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+public class CustomJwtAuthenticationConverter extends JwtAuthenticationConverter {
+}
